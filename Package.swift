9afdb49e4494dd4cd15cc23b52ceb2be5607ff42
guoqing <guoqing@geetest.com>
2024-04-08 18:04:42 +0800

    first commit

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "gt3-swift-package",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "gt3-swift-package",
+            targets: ["GT3Captcha"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        
+        .binaryTarget(name: "GT3Captcha",
+                      path: "./Sources/GT3Captcha.xcframework"),
+        
+            .target(name: "GT3Captcha",
+                    resources: [.copy("./Sources/PrivacyInfo.xcprivacy"),
+                                .copy("./Sources/GT3Captcha.bundle")])
+        
+    ]
+)
